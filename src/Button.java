c385f9e5b49db7b1c5d0126e541e1b5b31157317
austin89 <austin89@uga.edu>
2014-03-17 15:43:19 -0400

    v3
    Moved JFRAME from Graphics (out of the main method) to Button where it
    can be accessed to repaint when the button is pressed

--- src/Button.java
+++ src/Button.java
@@ -5,27 +5,40 @@ import java.awt.*;
 
 
 public class Button extends JPanel{
-	private JButton push;
-	private JPanel panel;
+	private JButton push;		//new Button
+	private JPanel panel;		//new panel for the button
+	private JFrame frame;		//new frame for the panel and button to be displayed in
+	
 	
 	public Button(){
-		push = new JButton("Initiate Game!");
-		push.addActionListener(new ButtonListener());
+		push = new JButton("Initiate Game!");			//sets the text displayed on the button
+		push.addActionListener(new ButtonListener());	//adds new action to be performed when the button is pressed
 		
 		panel = new JPanel();
-		BoxLayout layout = new BoxLayout(panel, BoxLayout.Y_AXIS);
-		panel.setLayout(layout);
+		BoxLayout layout = new BoxLayout(panel, BoxLayout.Y_AXIS);		
+		panel.setLayout(layout);						//sets the alignment of the JFRAME window
 		
-		add(push);
+		add(push);										//adds the button to the frame window
 		
-		setBackground(Color.LIGHT_GRAY);
-		//setPreferredSize(new Dimension(400, 300));
+		setBackground(Color.LIGHT_GRAY);				//sets the color scheme of the initial window
+		//setPreferredSize(new Dimension(400, 300));	//sets the size of the initial window
 	}
 
+	
+	public void showFrame(){
+		frame = new JFrame("Let's make some magic!");			//displays words on top of window
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//guarantees that the app exits when pressing X
+		
+		frame.getContentPane().add(new Slider());				//adds the level slider to the window
+		
+		frame.pack();
+		frame.setVisible(true);									//displays the application
+	}
+	
 	private class ButtonListener implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
-			Countdown timer = new Countdown();
-			timer.createAndShowUI();
+			Countdown timer = new Countdown();		//creates a new Countdown object
+			timer.createAndShowUI();				//displays the countdown timer (but still in a separate window)
 		}
 	}
 
--- src/Graphics.java
+++ src/Graphics.java
@@ -5,25 +5,15 @@ public class Graphics {
 
 	public static void main(String[] args) {
 		
+		Button panel = new Button();
+		panel.showFrame();
 
-		JFrame frame = new JFrame("Let's make some magic!");	//displays words on top of window
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	//guarantees that the app exits when pressing X
 /*		
 		JPanel panel = new JPanel();							//instantiates new JPanel
 		panel.setPreferredSize (new Dimension(480, 500));		//sets the size of the JPanel to 480 width and 500 height
 		panel.setBackground(Color.GREEN);
 		frame.getContentPane().add(panel);						//gets the JFrame and adds the 480x500 screen size
 */		
-		
-		
-		//Button pushButton = new Button();
-		//frame.getContentPane().add(pushButton);
-		
-		frame.getContentPane().add(new Slider());
-
-		frame.pack();
-		frame.setVisible(true);									//displays the application
-		
 	}
 
 }
