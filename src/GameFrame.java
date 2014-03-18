be59ef6a30b417678b83b17ffbbab871dd4d5615
austin89 <austin89@uga.edu>
2014-03-17 20:44:30 -0400

    v4 (Displays a window with shapes, but you can't click them)

---
+++ src/GameFrame.java
@@ -0,0 +1,43 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class GameFrame extends JPanel{
+	JPanel panel;
+	JLabel label;
+	
+	
+	public GameFrame(){
+		//panel = new JPanel();
+		//panel.setVisible(false);		//make sure this is the last line AFTER you add the pane
+	}
+	
+	public static void showGame(){
+		JFrame frame = new JFrame("Shapes");
+		frame.getContentPane().add(new GameFrame());
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 600);		
+		frame.setVisible(true);		//make sure this is the last line AFTER you add the pane
+	}
+
+	
+	public void paintComponent(Graphics g)  
+    {  
+       super.paintComponent(g); 					//call superclass's paintComponent 
+       Paint p = new Paint();
+       //RECTANGLE
+       //(X-VALUE, Y-VALUE, WIDTH, HEIGHT)
+       g.setColor(Color.RED);						//sets color of the rectangle
+       g.fillRect(p.setX(), p.setY(), 100, 100);	//draws filled in rectangle
+       
+       //OVAL
+       //(X-VALUE, Y-VALUE, WIDTH, HEIGHT)
+       g.setColor(Color.BLUE);
+       g.fillOval(p.setX(), p.setY(), 100, 150);	//draws outline of a circle
+
+    }
+	
+}
